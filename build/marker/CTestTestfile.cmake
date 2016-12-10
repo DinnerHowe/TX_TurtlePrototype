b3482fcb24ac6df47073ae0027e2ebd51f344df5
DinnerHowe <xu001314@163.com>
2016-12-10 18:45:09 +0800

    remove build devel

--- build/marker/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/howe/turtlebot_demo/src/marker
-# Build directory: /home/howe/turtlebot_demo/build/marker
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
